52bbfe18b9985df285d7ea1c0b8253358b271173
Samieh Abdeljabbar <samieh@Samiehs-MBP.attlocal.net>
2020-08-03 22:10:15 -0500

    commit

--- practice/index2.js
+++ practice/index2.js
@@ -5,3 +5,19 @@ var number =5; //this is how you declare a variable
 /* Data Types: 
 undefined, null, boolean, string, symbol, number and object 
 */ 
+
+ // var declaring a variable
+
+var myName = "Samieh" //variables can change as we go through the program
+
+let ourName = "freeCodeCamp"
+
+const pi = 3.14 //const can never change
+
+var a;
+var b = 2;
+
+a =7;
+b = a; //assigned the contents of a to b
+
+console.log(a)
