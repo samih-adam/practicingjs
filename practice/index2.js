27a19cf57ffd582f762721ca5a3a23707bf96371
Samieh Abdeljabbar <samieh@Samiehs-MBP.attlocal.net>
2020-08-03 22:02:48 -0500

    commit

--- practice/index2.js
+++ practice/index2.js
@@ -1,3 +1,7 @@
 // is in inline comment that you can use. just do the forward slash twice
 
 var number =5; //this is how you declare a variable
+
+/* Data Types: 
+undefined, null, boolean, string, symbol, number and object 
+*/ 
